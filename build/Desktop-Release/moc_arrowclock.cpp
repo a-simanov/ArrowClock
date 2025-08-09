6c25dc28da8def8d187115d1e4ab8795c3cb912a
a-simanov <metall704@yandex.ru>
2025-08-09 17:29:18 +0300

    refactoring

---
+++ build/Desktop-Release/moc_arrowclock.cpp
@@ -0,0 +1,156 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'arrowclock.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.4.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../arrowclock.h"
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'arrowclock.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.4.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+namespace {
+struct qt_meta_stringdata_ArrowClock_t {
+    uint offsetsAndSizes[14];
+    char stringdata0[11];
+    char stringdata1[19];
+    char stringdata2[1];
+    char stringdata3[18];
+    char stringdata4[4];
+    char stringdata5[12];
+    char stringdata6[26];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_ArrowClock_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_ArrowClock_t qt_meta_stringdata_ArrowClock = {
+    {
+        QT_MOC_LITERAL(0, 10),  // "ArrowClock"
+        QT_MOC_LITERAL(11, 18),  // "showHideChangeTime"
+        QT_MOC_LITERAL(30, 0),  // ""
+        QT_MOC_LITERAL(31, 17),  // "slotMenuRequested"
+        QT_MOC_LITERAL(49, 3),  // "pos"
+        QT_MOC_LITERAL(53, 11),  // "dialogTheme"
+        QT_MOC_LITERAL(65, 25)   // "on_btn_stop_alarm_clicked"
+    },
+    "ArrowClock",
+    "showHideChangeTime",
+    "",
+    "slotMenuRequested",
+    "pos",
+    "dialogTheme",
+    "on_btn_stop_alarm_clicked"
+};
+#undef QT_MOC_LITERAL
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_ArrowClock[] = {
+
+ // content:
+      10,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       4,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   38,    2, 0x08,    1 /* Private */,
+       3,    1,   39,    2, 0x08,    2 /* Private */,
+       5,    0,   42,    2, 0x08,    4 /* Private */,
+       6,    0,   43,    2, 0x08,    5 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QPoint,    4,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject ArrowClock::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_ArrowClock.offsetsAndSizes,
+    qt_meta_data_ArrowClock,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_ArrowClock_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<ArrowClock, std::true_type>,
+        // method 'showHideChangeTime'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'slotMenuRequested'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QPoint, std::false_type>,
+        // method 'dialogTheme'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_btn_stop_alarm_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void ArrowClock::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<ArrowClock *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->showHideChangeTime(); break;
+        case 1: _t->slotMenuRequested((*reinterpret_cast< std::add_pointer_t<QPoint>>(_a[1]))); break;
+        case 2: _t->dialogTheme(); break;
+        case 3: _t->on_btn_stop_alarm_clicked(); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject *ArrowClock::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *ArrowClock::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_ArrowClock.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int ArrowClock::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 4)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 4;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 4)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 4;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
---
+++ build/Desktop-Release/ui_arrowclock.h
@@ -0,0 +1,117 @@
+/********************************************************************************
+** Form generated from reading UI file 'arrowclock.ui'
+**
+** Created by: Qt User Interface Compiler version 6.4.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_ARROWCLOCK_H
+#define UI_ARROWCLOCK_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_ArrowClock
+{
+public:
+    QAction *action_change_time;
+    QAction *action_dark_theme;
+    QAction *action_setAlarm;
+    QWidget *centralwidget;
+    QGridLayout *gridLayout;
+    QPushButton *btn_stop_alarm;
+    QSpacerItem *verticalSpacer;
+    QLabel *lbl_alarm;
+    QStatusBar *statusbar;
+    QMenuBar *menuBar;
+    QMenu *menu;
+
+    void setupUi(QMainWindow *ArrowClock)
+    {
+        if (ArrowClock->objectName().isEmpty())
+            ArrowClock->setObjectName("ArrowClock");
+        ArrowClock->resize(800, 600);
+        action_change_time = new QAction(ArrowClock);
+        action_change_time->setObjectName("action_change_time");
+        action_change_time->setCheckable(false);
+        action_dark_theme = new QAction(ArrowClock);
+        action_dark_theme->setObjectName("action_dark_theme");
+        action_dark_theme->setCheckable(false);
+        action_setAlarm = new QAction(ArrowClock);
+        action_setAlarm->setObjectName("action_setAlarm");
+        centralwidget = new QWidget(ArrowClock);
+        centralwidget->setObjectName("centralwidget");
+        gridLayout = new QGridLayout(centralwidget);
+        gridLayout->setObjectName("gridLayout");
+        btn_stop_alarm = new QPushButton(centralwidget);
+        btn_stop_alarm->setObjectName("btn_stop_alarm");
+
+        gridLayout->addWidget(btn_stop_alarm, 2, 0, 1, 1);
+
+        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        gridLayout->addItem(verticalSpacer, 0, 0, 1, 1);
+
+        lbl_alarm = new QLabel(centralwidget);
+        lbl_alarm->setObjectName("lbl_alarm");
+        QFont font;
+        font.setBold(true);
+        lbl_alarm->setFont(font);
+        lbl_alarm->setLayoutDirection(Qt::LeftToRight);
+
+        gridLayout->addWidget(lbl_alarm, 1, 0, 1, 1);
+
+        ArrowClock->setCentralWidget(centralwidget);
+        statusbar = new QStatusBar(ArrowClock);
+        statusbar->setObjectName("statusbar");
+        ArrowClock->setStatusBar(statusbar);
+        menuBar = new QMenuBar(ArrowClock);
+        menuBar->setObjectName("menuBar");
+        menuBar->setGeometry(QRect(0, 0, 800, 23));
+        menu = new QMenu(menuBar);
+        menu->setObjectName("menu");
+        ArrowClock->setMenuBar(menuBar);
+
+        menuBar->addAction(menu->menuAction());
+        menu->addAction(action_change_time);
+        menu->addAction(action_dark_theme);
+        menu->addAction(action_setAlarm);
+
+        retranslateUi(ArrowClock);
+
+        QMetaObject::connectSlotsByName(ArrowClock);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *ArrowClock)
+    {
+        ArrowClock->setWindowTitle(QCoreApplication::translate("ArrowClock", "ArrowClock", nullptr));
+        action_change_time->setText(QCoreApplication::translate("ArrowClock", "\320\243\321\201\321\202\320\260\320\275\320\276\320\262\320\270\321\202\321\214 \320\262\321\200\320\265\320\274\321\217", nullptr));
+        action_dark_theme->setText(QCoreApplication::translate("ArrowClock", "\320\222\321\213\320\261\321\200\320\260\321\202\321\214 \321\202\320\265\320\274\321\203", nullptr));
+        action_setAlarm->setText(QCoreApplication::translate("ArrowClock", "\320\243\321\201\321\202\320\260\320\275\320\276\320\262\320\270\321\202\321\214 \320\261\321\203\320\264\320\270\320\273\321\214\320\275\320\270\320\272", nullptr));
+        btn_stop_alarm->setText(QCoreApplication::translate("ArrowClock", "\320\236\321\201\321\202\320\260\320\275\320\276\320\262\320\270\321\202\321\214 \320\261\321\203\320\264\320\270\320\273\321\214\320\275\320\270\320\272", nullptr));
+        lbl_alarm->setText(QCoreApplication::translate("ArrowClock", "<html><head/><body><p align=\"center\"><span style=\" font-size:16pt; font-weight:700;\">TextLabel</span></p></body></html>", nullptr));
+        menu->setTitle(QCoreApplication::translate("ArrowClock", "\320\234\320\265\320\275\321\216", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class ArrowClock: public Ui_ArrowClock {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_ARROWCLOCK_H
